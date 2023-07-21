91e90cabc77bdd2dc0df89a34c7890d215418ff8
Yahya LAZREK <63449913+UUinc@users.noreply.github.com>
2023-07-21 17:14:06 +0100

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import express from 'express';
+import http from 'http';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import compression from 'compression';
+import cors from 'cors';
+import mongoose from 'mongoose';
+
+import router from './router';
+
+const app = express();
+
+app.use(cors({
+    credentials: true,
+}));
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+const server = http.createServer(app);
+
+server.listen(3000, ()=>{
+    console.log('server running on http://localhost:3000/');
+});
+
+const MONGO_URL = 'mongodb+srv://yahya:VH8pVr5UUBX6iYkn@floorplan.cpwtmhv.mongodb.net/?retryWrites=true&w=majority';
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on('error', (error: Error) => console.log(error));
+
+app.use('/', router());
